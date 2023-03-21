dc0cd2876002950779ff11e7d2260d0c0dc1654c
585090 <simen@hhnet.no>
2023-03-21 11:29:17 +0100

    Oppdatert startside for boligkjop

--- bachelor/src/App.jsx
+++ bachelor/src/App.jsx
@@ -2,27 +2,12 @@ import styled from "styled-components";
 import SimulertBoligkjop from "./SimulertBoligkjøp";
 
 const Bakgrunn = styled.body`
-  background-color: #FAF2E7;
-  height: 100%;
-`;
-
-const Overskift = styled.div`
-  text-align: center;
-  font: Tobias;
-`;
-
-
-const UndertekstDiv = styled.div`
-  text-alig: center;
-  float: center;
+  background-color: white;
 `;
 
 function App() {
   return (
     <Bakgrunn>
-     <Overskift>
-        <h1>Simuler et kjøp av drømmeboligen din</h1>
-     </Overskift>
      <SimulertBoligkjop />
     </Bakgrunn>
   );
