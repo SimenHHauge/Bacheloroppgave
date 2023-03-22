62fc9e27dc474c698ca90d6baa249e9257d5a61f
585090@stud.hvl.no <585090@stud.hvl.no>
2023-03-22 13:10:30 +0100

    Første utkast med brukbare linker

--- bachelor/src/SimulertBoligkjop/Navigasjon.jsx
+++ bachelor/src/SimulertBoligkjop/Navigasjon.jsx
@@ -1,11 +1,37 @@
 import React from "react";
+import styled from "styled-components"; 
+
+import Hus from "./Bilder/Hus.png"
+
+const Footer = styled.footer`
+    position: fixed;
+    bottom: 0;
+    width: 80%;
+`;
+
+const Navbar = styled.ul`
+    list-style-type: none;
+    margin: 10px;
+`;
+
+const NavItems = styled.li`
+    display: inline;
+    float: center;
+`;
 
 const Navigasjon = () => {
     return (
-        <div>
-            <a href="./">Hjem</a>
-            <a href="./SimulertBoligkjop">Simulasjon</a>
-        </div>
+        <Footer>
+            <Navbar>
+                <NavItems>
+                    <a href="./">Hjem</a>
+                </NavItems>
+                <NavItems>
+                    <a href="./SimulertBoligkjop"><img src={Hus} /></a>
+                </NavItems>
+            </Navbar>
+
+        </Footer>
     )
 }
 
